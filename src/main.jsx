f45ae5a2e2cdf2b28912d1fae8acecc5ee5457c4
SMcGregor199 <shaynemcgregor1@gmail.com>
2026-04-24 10:27:31 -0400

    Revert "add 5th track"
    This reverts commit 30ed20a75ed8eb484071f4e7fb0d655c50ae058c.

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,19 @@
 import {createRoot} from "react-dom/client"
 import heroPng from "./assets/hero.png";
 const root = createRoot(document.querySelector("#root"))
-const tracks = <ul>
-  <li>Track 1</li>
-  <li>Track 2</li>
-  <li>Track 3</li>  
-  <li>Track 4</li>
-  <li>Track 5</li>
-</ul>
-console.log(tracks);
+
 function Main () {
   return(
     <main>
       <h1>Welcome to {import.meta.env.VITE_NEW_TITLE}</h1>
       <img src={heroPng} alt="Hero" />
       <p>What's your favorite lofi track?</p>
-      {tracks}
+      <ul>
+        <li>Track 1</li>
+        <li>Track 2</li>
+        <li>Track 3</li>  
+        <li>Track 4</li>
+      </ul>
     </main>
   )
 }
